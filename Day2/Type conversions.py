003a301eee8c9c7f4751bb54d9d63c87b8e10433
madhu-d8 <madumita0805@gmail.com>
2026-05-20 20:44:24 +0530

    covered  Data tyes,mathematical opeations,pemdas,type keyword,fstrings,type convertions

---
+++ Day2/Type conversions.py
@@ -0,0 +1,71 @@
+#type error
+
+# len(12345)
+
+#type error
+# print(type(123))
+# print(type("Hello"))
+# print(type(3.14))
+# print(type(True))
+#
+# IF WE WNT TO TURN STR INTO INTEGER
+#
+# print(int("123")+ int("456"))
+# print(int("abc")+ int("456"))#valuse error
+
+# name = input("enter your name: ")
+# length_name = len(name)
+# print(type("number of letters in your name : "))
+# print(type(length_name))
+# print("number of letters in your name : " + str(length_name))
+
+a = 25
+b = 4.5
+c = "Python"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+Roll_no = 5
+grade = 9.0
+name = "manoj"
+is_student = True
+print(Roll_no)
+print(grade)
+print(name)
+print(is_student)
+
+number = 100
+print(type(number))
+
+age = 20
+print("Age is " , age)
+
+age = 19
+print("Age is " + str(age))
+
+num = 50
+num_float = float(num)
+print(type(num))
+print(float(num))
+print(type(num_float))
+
+price = 99.99
+price_int = int(price)
+print(price_int)
+print(type(price_int))
+
+num = "250"
+num_int = int(num)
+add = num_int + 50
+print(add)
+
+is_working = True
+print(type(is_working))
+
+num1 = "100"
+num2 = 50
+
+print(int(num1) + num2)
---
+++ Day2/projectDay2.py
@@ -0,0 +1,10 @@
+print("welcome to the tip calaclator!")
+bill = float(input("what is the total bill? $"))
+tip = int(input("what percentage tip would you like to give? 10 12 15"))
+people = int(input("how many people to split the bill?"))
+tip_as_percentage = tip/100
+total_tip_amount = bill*tip_as_percentage
+total_bill = total_tip_amount + total_tip_amount
+bill_per_person = total_bill/people
+final_amount = round(bill_per_person,2)
+print(f"each person should pay: ${final_amount}")
